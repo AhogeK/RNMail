a3064cc9c116103b0031971936c5084647c6d107
AhogeK <ahogek@gmail.com>
2022-10-13 16:58:50 +0800

    fix: add SafeAreaView make iphone header bar great view

--- src/components/header-bar.tsx
+++ src/components/header-bar.tsx
@@ -1,21 +1,24 @@
 import { Bar } from "@/atoms";
 import AnimatedBox, { AnimatedBoxProps } from "@/atoms/animated-box";
 import * as React from "react";
+import { SafeAreaView } from "react-native";
 
 const HeaderBar: React.FC<AnimatedBoxProps> = ({ children, ...rest }: any) => {
   return (
     <AnimatedBox position="absolute" top={0} left={0} right={0} {...rest}>
-      <Bar
-        variant="headerBar"
-        flexDirection="row"
-        alignItems="center"
-        mx="lg"
-        my="md"
-        px="sm"
-        minHeight={44}
-      >
-        {children}
-      </Bar>
+      <SafeAreaView>
+        <Bar
+          variant="headerBar"
+          flexDirection="row"
+          alignItems="center"
+          mx="lg"
+          my="md"
+          px="sm"
+          minHeight={44}
+        >
+          {children}
+        </Bar>
+      </SafeAreaView>
     </AnimatedBox>
   );
 };
